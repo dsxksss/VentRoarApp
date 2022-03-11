356dc683b3702159465b98ab5bc5e7aa468bdcd5
图灵猫 <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-03-11 17:35:13 +0800

    add:daisyui color items

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,21 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#e5e7eb",
+          secondary: "#10b981",
+          accent: "#e11d48",
+          neutral: "#1c1917",
+          "base-100": "#1f2937",
+          info: "#38bdf8",
+          success: "#4ade80",
+          warning: "#f97316",
+          error: "#dc2626",
+        },
+      },
+    ],
+  },
 };
