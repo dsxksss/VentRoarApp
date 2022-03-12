5cfbaa0a4c39105fe3d309a0b0a4b67dd871a64e
dsxksss <9554230+xiao-kang-zhang@user.noreply.gitee.com>
2022-03-12 19:28:07 +0800

    add:darggable do

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -7,12 +7,20 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import { useParams } from "react-router";
+import Draggable from "react-draggable";
 import NavImg from "./../components/NavImg";
 import ToDark from "./../components/ToDark";
+
 import "./Page.css";
+import { useRef } from "react";
 
 const Page: React.FC = () => {
   const { name } = useParams<{ name: string }>();
+  //因为Draggable会操纵原始DOM,但是React不建议这样做,所以可以给它传递一个ref,以避免警告
+  const nodeRef = useRef(null);
+  const handleStart = () => console.log("handleStart");
+  const handleDrag = () => console.log("handleDrag");
+  const handleStop = () => console.log("handleStop");
 
   return (
     <IonPage>
@@ -32,17 +40,30 @@ const Page: React.FC = () => {
 
       <IonContent>
         <div className="flex justify-center items-center h-[91vh]">
-          <div className="mockup-code mt-[-9vh] min-w-[91vw] flex flex-col">
-            <pre data-prefix="$">
-              <code>开始安装 {name} 页面。</code>
-            </pre>
-            <pre data-prefix=">" className="text-warning">
-              <code>{name} 正在安装中......</code>
-            </pre>
-            <pre data-prefix=">" className="text-success">
-              <code>{name} 安装成功!</code>
-            </pre>
-          </div>
+          <Draggable
+            axis="both" //拖动的方向
+            grid={[1, 1]} //每次拖动应该移动多少,C1:X,C2:Y
+            scale={3} //摩擦度数,越高越难拖动
+            onStart={handleStart} //开始拖动发生的事情
+            onDrag={handleDrag} //拖动时发生的事情
+            onStop={handleStop} //结束拖动发生的事情
+            nodeRef={nodeRef} //传递ref避免报错警告
+          >
+            <div
+              ref={nodeRef} //传递ref避免报错警告
+              className="mockup-code mt-[-9vh] min-w-[91vw] flex flex-col"
+            >
+              <pre data-prefix="$">
+                <code>开始安装 {name} 页面。</code>
+              </pre>
+              <pre data-prefix=">" className="text-warning">
+                <code>{name} 正在安装中......</code>
+              </pre>
+              <pre data-prefix=">" className="text-success">
+                <code>{name} 安装成功!</code>
+              </pre>
+            </div>
+          </Draggable>
         </div>
       </IonContent>
     </IonPage>
